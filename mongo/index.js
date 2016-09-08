9f711ba7303b0c7cd07e2a93ca0a648532c8d9eb
Junseok Oh <kotohana5706@edcan.kr>
2016-09-09 02:20:28 +0900

    [Mongo] Update Database Schema

--- mongo/index.js
+++ mongo/index.js
@@ -2,12 +2,12 @@ var mongoose = require('mongoose');
 var db = mongoose.connect('mongodb://localhost/Safood');
 
 var userSchema = mongoose.Schema({
-    apikey: {type: String},
-    username: {type: String},
-    email: {type: String},
     userid: {type: String},
-    groupid: {type: String},
+    username: {type: String},
     password: {type: String},
+    apikey: {type: String},
+    groupid: {type: String},
+    history: {type: String},
     exception: {
         religion: {type: Array},
         allergy: {type: Array},
@@ -15,13 +15,37 @@ var userSchema = mongoose.Schema({
     }
 });
 
-var groupSchema = mongoose.Schema({
+var userGroupSchema = mongoose.Schema({
     name: {type: String},
-    id: {type: String},
-    members: {type: Array},
+    groupid: {type: String},
+    admin: {type: String},
+    members: {type: Array}
+});
+
+var foodSchema = mongoose.Schema({
+    name: {type: String},
+    weight: {type: String},
+    weightUnit: {type: String},
+    barcode: {type: String},
+    foodType: {type: String},
+    foodAllergic: {type: String},
+    foodIngredient: {type: String},
+    foodCalorie: {type: String},
+    foodOnceIntake: {type: String}
+});
+
+var safoodGroupSchema = mongoose.Schema({
+    name: {type: String},
+    admin: {type: String},
+    lastUpdate: {type: Date},
+    foodList: {type: Array}
 });
 
 var User = mongoose.model(userSchema);
-var Group = mongoose.model(groupSchema);
+var UserGroup = mongoose.model(userGroupSchema);
+var Food = mongoose.model(foodSchema);
+var SafoodGroup = mongoose.model(safoodGroupSchema);
 exports.User = User;
-exports.Group = Group;
+exports.UserGroup = UserGroup;
+exports.Food = Food;
+exports.SafoodGroup = SafoodGroup;
