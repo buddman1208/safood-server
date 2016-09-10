132030e81278363ffa4e7af0fd4e67f0102a3c94
Junseok Oh <kotohana5706@edcan.kr>
2016-09-11 03:31:35 +0900

    [User] /user/destroySelf

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,13 @@
 module.exports = auth;
 
 function user(app, db, randomStr) {
+    app.post('/user/destroySelf', function(req, res){
+        if(req.body.apikey != undefined && req.body.apikey != null){
+            db.User.remove({apikey:apikey}, function(err, numAff){
+                if(err) {
+                    throw err;
+                } else res.sendStatus(200);
+            })
+        } else res.sendStatus(403);
+    });
 }
